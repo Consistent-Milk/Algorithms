66fa090b638bc4c68c4dc8e1d6db4e7d1fa7fb95
Mahmudul Hasan <mahmudulhasanfh91@gmail.com>
2022-05-24 20:20:18 +0600

    Seperated Dynamic Connectivity Related Algorithms

--- Dynamic Connectivity Problem/QuickFind/QuickFind.py
+++ Dynamic Connectivity Problem/QuickFind/QuickFind.py
@@ -28,7 +28,7 @@ class QuickFind:
             else:
                 return False 
         else:
-            raise ValueError(f"The value of p and q must be in the interval [0,{n}]")
+            raise ValueError(f"The value of p and q must be and integer in the interval [0,{n}]")
 
     def union(self, p: int, q: int) -> None:
         n = self.number_of_elements - 1
@@ -40,7 +40,7 @@ class QuickFind:
                 if (self.id_array[i] == pid):
                     self.id_array[i] = qid
         else:
-            raise ValueError(f"The value of p and q must be in the interval [0,{n}]")
+            raise ValueError(f"The value of p and q must be an integer in the interval [0,{n}]")
 
 if __name__ == "__main__":
     main()
