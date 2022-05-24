b6f1103db1e96a88f67ac2c4f210134f95329d13
Mahmudul Hasan <mahmudulhasanfh91@gmail.com>
2022-05-24 20:15:11 +0600

    Seperated Dyanmic Connectivity Related Algorithms

---
+++ Dynamic Connectivity Problem/QuickFind/QuickFind.py
@@ -0,0 +1,46 @@
+def main():
+    n = 6
+    QuickFind_instance = QuickFind(n)
+    print(f"The initial array is : {QuickFind_instance.id_array}")
+    QuickFind_instance.union(1,2)
+    print(f"Array after applying union(1,2): {QuickFind_instance.id_array}")
+    QuickFind_instance.union(1,3)
+    print(f"Array after applying union(1,3): {QuickFind_instance.id_array}")
+    print(f"Comment: connected(2,3) should now return true.")
+    print(f"connected(2,3) returns {str(QuickFind_instance.connected(2,3))}")
+    print(f"Comment: connected(2,5) should now return false.")
+    print(f"connected(2,5) returns {str(QuickFind_instance.connected(2,5))}")
+    print(f"Comment: connected(2,6) should raise a ValueError")
+    QuickFind_instance.connected(2,6)
+
+class QuickFind:
+
+    def __init__(self, number_of_elements:int) -> None:
+        self.number_of_elements = number_of_elements
+
+        self.id_array = list(range(self.number_of_elements))
+
+    def connected(self, p: int, q: int) -> bool:
+        n = self.number_of_elements - 1
+        if (0<=p<=n) and (0<=q<=n):
+            if self.id_array[p] == self.id_array[q]:
+                return True
+            else:
+                return False 
+        else:
+            raise ValueError(f"The value of p and q must be in the interval [0,{n}]")
+
+    def union(self, p: int, q: int) -> None:
+        n = self.number_of_elements - 1
+        if (0<=p<=n) and (0<=q<=n):
+            pid = self.id_array[p]
+            qid = self.id_array[q]
+
+            for i in range(0, len(self.id_array)):
+                if (self.id_array[i] == pid):
+                    self.id_array[i] = qid
+        else:
+            raise ValueError(f"The value of p and q must be in the interval [0,{n}]")
+
+if __name__ == "__main__":
+    main()
--- Dynamic Connectivity Problem/QuickUnion/QuickUnion.py
+++ Dynamic Connectivity Problem/QuickUnion/QuickUnion.py
@@ -1,3 +1,5 @@
+# Fixed bugs with raising proper value errors
+
 def main():
     n = int(input('Input a positive integer n for the length of the initial array: '))
     id_array = QuickUnion(n)
@@ -19,17 +21,17 @@ class QuickUnion():
         self.id_array = list(range(self.number_of_elements))
 
     def root(self, element: int) -> int:
-        n = self.number_of_elements
+        n = self.number_of_elements - 1
         if (0 <= element <= n):
             while (element != self.id_array[element]):
                 element = self.id_array[element]
             return element
         else:
             raise ValueError(
-                f'The element to look up root must be between 0 and {n}')
+                f'The element to look up root for must be an integer in the interval [0, {n}]')
 
     def connected(self, element_1: int, element_2: int) -> bool:
-        n = self.number_of_elements
+        n = self.number_of_elements - 1
         if (0 <= element_1 <= n) and (0 <= element_2 <= n):
             if self.root(element_1) == self.root(element_2):
                 return True
@@ -37,11 +39,11 @@ class QuickUnion():
                 return False
         else:
             raise ValueError(
-                f"The elements to check for connection must be between 0 and {n}")
+                f"The elements to check for connection must be an integer in the interval [0, {n}]")
 
     def union(self, element_1: int, element_2: int) -> None:
         # Change root of element_1 to root of element_2, i.e, connect them
-        n = self.number_of_elements
+        n = self.number_of_elements - 1
         if (0 <= element_1 <= n) and (0 <= element_2 <= n):
             i = self.root(element_1)
             j = self.root(element_2)
@@ -49,7 +51,7 @@ class QuickUnion():
             self.id_array[i] = j
         else:
             raise ValueError(
-                f"The elements to connect must be between 0 and {n}")
+                f"The elements to connect must be an integer in the interval [0, {n}]")
 
 
 if __name__ == "__main__":
