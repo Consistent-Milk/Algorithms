ba10765bc3938ca92d67e1e240a2d7a3d9be7b37
Consistent Milk <mahmudulhasanfh91@gmail.com>
2022-06-13 00:27:56 +0600

    Linked List Update

---
+++ LinkedLists/DoublyLinkedList.py
@@ -0,0 +1,20 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.previous = None
+        self.next = None
+
+    def __str__(self):
+        return f"{self.data}"
+
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def __iter__(self):
+        node = self.head
+        while node:
+            yield node.data
+            
---
+++ LinkedLists/readme.md
@@ -0,0 +1,6 @@
+Here several variations of the data structure 'Linked List' has been implemented.
+
+1. LinkedList.py: This script implements a singly linked list data structure. 
+
+2. LinkedList-Extended.py: This script implements an optimized form of a singly linked list data structure with more
+methods. 
